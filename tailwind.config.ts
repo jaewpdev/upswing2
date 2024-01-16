359a6b4dc76f75d3390a8588ff073dee8c2d5aed
Jae Park <jaewp94@gmail.com>
2024-01-16 08:47:14 -0500

    Initial set up complete.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,13 +5,23 @@ const config: Config = {
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    container: {
+      padding: "2rem",
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        brand: {
+          yellow: '#FFDF38',
+          orange: '#FF7C2E',
+          blue: '#428EC5'
+        }
+      },
+      fontFamily: {
+        sans: ["var(--font-inter)"],
+        mono: ["var(--font-inter)"],
       },
     },
   },
